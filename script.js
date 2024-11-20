187c2d8dc5f2051c572dee2f7ba5f4adbd20c720
Pamidimarri vijay sankar <sankerpamidimarri@gmail.com>
2024-11-20 20:10:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
+    // Get the values from the form elements using their name attributes
+    var firstName = document.forms["form1"]["fname"].value;
+    var lastName = document.forms["form1"]["lname"].value;
 
+    // Display the values using alert
+    alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
